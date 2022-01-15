28f4b02e0f8633a2fce1969da05f2449afd0fd3b
Robin Reiter <robin7331@gmail.com>
2022-01-15 08:47:55 +0100

    Add automatic gerber creation to build scriot

--- kikit-panelize.sh
+++ kikit-panelize.sh
@@ -1,10 +1,15 @@
-rm -rf panel-output
-mkdir panel-output
+rm -rf output
+mkdir output
 
 docker run  -w /kikit \
             -v $(pwd):/kikit \
             yaqwsx/kikit:nightly-m1 \
-            kikit panelize -p kikit-panel-preset.json pixel-pump-mainboard.kicad_pcb panel-output/pixel-pump-mainboard-panel.kicad_pcb
+            kikit panelize -p kikit-panel-preset.json pixel-pump-mainboard.kicad_pcb output/pixel-pump-mainboard-panel.kicad_pcb
+
+docker run  -w /kikit \
+            -v $(pwd):/kikit \
+            yaqwsx/kikit:nightly-m1 \
+            kikit fab jlcpcb --no-drc output/pixel-pump-mainboard-panel.kicad_pcb output
 
 
 
