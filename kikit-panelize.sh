9b504e506ec4c3932a9fe77a6835758e3d3fe8a0
Robin Reiter <robin7331@gmail.com>
2022-04-29 13:06:32 +0200

    Rename ports to EXP1 and EXP2 and reduce mousebite stiffness

--- kikit-panelize.sh
+++ kikit-panelize.sh
@@ -4,12 +4,16 @@ mkdir output
 docker run  -w /kikit \
             -v $(pwd):/kikit \
             yaqwsx/kikit:nightly-m1 \
-            kikit panelize -p kikit-panel-preset.json pixel-pump-mainboard.kicad_pcb output/pixel-pump-mainboard-panel.kicad_pcb
+            kikit panelize -p kikit-panel-preset.json \
+            pixel-pump-mainboard.kicad_pcb \
+            output/pixel-pump-mainboard-panel.kicad_pcb
 
 docker run  -w /kikit \
             -v $(pwd):/kikit \
             yaqwsx/kikit:nightly-m1 \
-            kikit fab jlcpcb --no-drc output/pixel-pump-mainboard-panel.kicad_pcb output
+            kikit fab jlcpcb --no-drc \
+            output/pixel-pump-mainboard-panel.kicad_pcb \
+            output
 
 
 
